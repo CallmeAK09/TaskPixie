d35caf92e82b138d739e871da266c83eb8057e12
Arjun AJ <aj110arjun@gmail.com>
2026-07-24 20:11:15 +0530

    feat: models Initiated

---
+++ config/settings/production.py
@@ -0,0 +1,30 @@
+from decouple import config
+
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    cast=lambda value: [host.strip() for host in value.split(",")]
+)
+
+# ==================================================
+# Security
+# ==================================================
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+SECURE_BROWSER_XSS_FILTER = True
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+X_FRAME_OPTIONS = "DENY"
+
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
